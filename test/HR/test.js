94b568d9b82926daaddcc2a72ce605eeca6c134b
Emmanuel Haguet <emmanuel.haguet@laposte.net>
2019-02-22 03:30:43 +0100

    HR iban bank code

--- test/HR/test.js
+++ test/HR/test.js
@@ -4,6 +4,10 @@ const assert = require('assert');
 const chai = require('chai')
 const validation = require('../../index')
 
+// TODO: check date
+// Documentation: https://www.hnb.hr/temeljne-funkcije/platni-promet/vodeci-brojevi-banaka
+// see to Vatican for check date
+
 // generated from the official HR website https://www.hnb.hr/temeljne-funkcije/platni-promet/iban-kalkulator
 it('HR generated IBAN from hnb.hr with invalid Account Number checksum', function() {
   // bankCode 4133006 : ok
@@ -20,5 +24,6 @@ it('HR IBAN with valid checksums', function() {
   // 1863000160
   chai.expect(validation.isValid('HR1210010051863000160')).to.equal(true)
   // from http://www.kaba.hr/en/iban-calculator/
+  // checked on https://www.hnb.hr/temeljne-funkcije/platni-promet/iban-kalkulator
   chai.expect(validation.isValid('HR5641330061863000160')).to.equal(true)
 });
